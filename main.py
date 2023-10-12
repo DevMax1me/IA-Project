b1db88c1ce5388ceae2326d8ee00ce3a6a113b2c
Maxime Badin <maxbadin38@gmail.com>
2023-10-12 15:16:03 +0200

    Wip

--- main.py
+++ main.py
@@ -5,40 +5,45 @@ from streamlit_drawable_canvas import st_canvas
 
 import keras
 import numpy as np
+import cv2
 
-# Create a canvas component
-model = keras.models.load_model('model_CNN_V6.h5')
+# load model.ckpt
+model = keras.models.load_model("maxime-V11.h5")
 
 st.header("Draw a number")
 st.subheader("Draw a number between 0 and 9")
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0)",  # Fixed fill color with some opacity
-    stroke_width=15,
-    stroke_color="#000",
-    background_color="#fff",
-    update_streamlit=True,
-    height=300,
-    width=300,
-    drawing_mode="freedraw",
-    key="canvas",
-)
 
-# Do something interesting with the image data and paths
+col1, col2 = st.columns(2)
+
+with col1:
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0)",  # Fixed fill color with some opacity
+        stroke_width=20,
+        stroke_color="#FFF",
+        background_color="#000",
+        update_streamlit=True,
+        height=300,
+        width=300,
+        drawing_mode="freedraw",
+        key="canvas",
+    )
+
+with col2:
+    if canvas_result.image_data is not None:
+        img = canvas_result.image_data.astype(np.uint8)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        resized = cv2.resize(gray, (28, 28), interpolation = cv2.INTER_AREA)
+
+        newimg = keras.utils.normalize(resized, axis = 1)
+
+        st.image(newimg, width=300)
+
+        newimg = np.array(newimg).reshape(-1, 28, 28, 1)
+
+
 if canvas_result.image_data is not None:
-    img = canvas_result.image_data.astype(np.uint8)
-    img = Image.fromarray(img)
+    predictions = model.predict(newimg)
     
-    # Invert the image
-    img = img.convert("L")
-
-    img = img.resize((28, 28))
-    img = np.array(img)
-    print(img.shape)
-    
-    img = img.reshape(1, 28, 28, 1)
-    st.image(img, width=150)
-    
-    prediction = model.predict(img)
-
-    st.write("## Prediction: ", np.argmax(prediction))
-    st.bar_chart(prediction[0])
+    st.write("## Prediction: ", np.argmax(predictions))
+    st.bar_chart(predictions[0])
